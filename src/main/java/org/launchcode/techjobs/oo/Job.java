e6ba6aa8f4c9cbdea4e62f02c54912e29f1ca7c2
Maddie <mddscudder44@gmail.com>
2022-05-05 15:06:01 -0400

    Added both constructors for Job.java

--- src/main/java/org/launchcode/techjobs/oo/Job.java
+++ src/main/java/org/launchcode/techjobs/oo/Job.java
@@ -13,13 +13,28 @@ public class Job {
     private PositionType positionType;
     private CoreCompetency coreCompetency;
 
-    // TODO: Add two constructors - one to initialize a unique ID and a second to initialize the
-    //  other five fields. The second constructor should also call the first in order to initialize
-    //  the 'id' field.
+    // TODO: Add two constructors - one to initialize a unique ID and a
+    //  second to initialize the other five fields. The second constructor
+    //  should also call the first in order to initialize the 'id' field.
+    public Job() {
+        id = nextId;
+        nextId++;
+    }
 
-    // TODO: Add custom equals and hashCode methods. Consider two Job objects "equal" when their id fields
-    //  match.
+    public Job(String name,Employer employer, Location location, PositionType positionType, CoreCompetency coreCompetency) {
+        this();
+        this.name = name;
+        this.employer = employer;
+        this.location = location;
+        this.positionType = positionType;
+        this.coreCompetency = coreCompetency;
+    }
 
-    // TODO: Add getters for each field EXCEPT nextId. Add setters for each field EXCEPT nextID
-    //  and id.
+
+    // TODO: Add custom equals and hashCode methods. Consider two Job
+    //  objects "equal" when their id fields match.
+
+
+    // TODO: Add getters for each field EXCEPT nextId. Add setters for
+    //  each field EXCEPT nextID and id.
 }
